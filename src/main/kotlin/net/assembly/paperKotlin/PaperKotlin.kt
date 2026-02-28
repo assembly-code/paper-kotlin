41f2c984e02c5d8bf7381df9050cb59df7d0b961
alexthegoood <mail.alexthegoood@gmail.com>
2026-02-28 20:02:06 +0200

    init

---
+++ src/main/kotlin/net/assembly/paperKotlin/PaperKotlin.kt
@@ -0,0 +1,14 @@
+package net.assembly.paperKotlin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PaperKotlin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
