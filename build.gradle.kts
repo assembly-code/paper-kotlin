510ff2dc106ee9b0802d90171a1758558b333503
alexthegoood <mail.alexthegoood@gmail.com>
2026-03-02 01:48:21 +0200

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 allprojects {
-    group = "com.github.assembly.paperkotlin"
+    group = "com.github.assembly.paperKotlin"
     description = "Kotlin library for simplify plugin development"
     version = "0.1.0"
 }
