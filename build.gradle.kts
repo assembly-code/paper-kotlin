f2e6d5979a293276c07873eb757198ac481c92ef
alexthegoood <mail.alexthegoood@gmail.com>
2026-03-02 19:34:11 +0200

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 allprojects {
     group = "com.github.assemblyDir.paperKotlin"
     description = "Kotlin library for simplify plugin development"
-    version = "1.0.0-snapshot.2"
+    version = "1.0.0-snapshot.3"
 }
